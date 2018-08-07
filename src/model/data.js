2323f940bbcffdaf26116e6eba0c861e678ea3b9
USER T430 - 12 <yamileth_2005@hotmail.com>
2018-08-07 13:17:07 -0500

    insertando fx aún por terminar

--- src/model/data.js
+++ src/model/data.js
@@ -8,15 +8,24 @@ validatorNameUser = (name) => {
         console.log('false')
         return false;
     }
-
 };
 
 
-  // window.validatorNameUser = (name) => {
-  //   if ((/^([A-Za-z0-9\s]{8,})+$/g.test(name))) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  // };
+validateContentOfpublications = (descriptionPostValue) => {
+    if (/^(?!\s)/.test(descriptionPostValue) && /^([A-Za-z0-9\s]{1,})/g.test(descriptionPostValue)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  /* *******************************************FUNCIONES DE VALIDACIÓN*******************************/
+  
+  window.validatorEmail = (email) => {
+    if (/^([a-zA-Z0-9._-]{3,})+@([a-zA-Z0-9.-]{5,})+\.([a-zA-Z]{2,})+$/.test(email)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
 
