32e91df5dc21ee1019b56786cf6dc322af3d11bb
Sol Lavarello <lavarellosolange@gmail.com>
2018-08-07 23:34:40 -0500

    sube datos de visitante a firebase

--- src/controller/main.js
+++ src/controller/main.js
@@ -1,43 +1,35 @@
 const btnRegister = document.getElementById('btn-register-visitant');
 const formRegisterVisitant = document.getElementById('register-visitant');
+const takePhoto = document.getElementById('create-photo')
 
 let video = document.getElementById('video');
 let canvas = document.getElementById('canvas');
 let btnCamera = document.getElementById('btn-camera');
 let img = document.getElementById('img-visitant');
 let context = canvas.getContext('2d');
-
+let cardVisit = document.getElementById('cardVIsit')
 
 btnRegister.addEventListener('click', () => {
-	// let nameVisitant = document.getElementById('name-visitant');
-	// let dniVisitant = document.getElementById('dni-visitant');
-	// let searchVisitant = document.getElementById('search-visitant');
-	// let reasonVisitant = document.getElementById('reason-visitant');
+	let nameVisitant = document.getElementById('name-visitant');
+	let typeDocument = document.getElementById('option-document')
+	let dniVisitant = document.getElementById('dni-visitant');
+	let companyVisitant = document.getElementById('company-visitant');
+	let searchVisitant = document.getElementById('search-visitant');
+	let reasonVisitant = document.getElementById('reason-visitant');
+	window.createVisitantInFirebase(nameVisitant, typeDocument, dniVisitant, companyVisitant, searchVisitant, reasonVisitant);
 	formRegisterVisitant.style.display = 'none'
-
-	// window.createVisitantInFirebase(nameVisitant, dniVisitant, searchVisitant, reasonVisitant);
-	// formRegisterVisitant.reset();
+	takePhoto.style.display = 'none';
+	btnRegister.style.display = 'none';
+	cardVisit.innerHTML = confirmationVisitant;
 })
 
 
-window.onload = () => {
-	document.getElementById('create-photo').style.display = 'none';
-
-}
-
-/*btnCamera.addEventListener('click', ()=>{
-    save(canvas,img);
-})*/
-
 document.getElementById('active-camera').addEventListener('click', () => {
-	document.getElementById('create-photo').style.display = 'block';
+	takePhoto.style.display = 'block';
 	viewImageCamera(video, canvas, img, btnCamera);
 	formRegisterVisitant.style.display = 'none'
 })
 
-// document.getElementById('check-email').addEventListener('touchmove', () => {
-
-// })
 
 // Trigger photo take
 document.getElementById("btn-camera").addEventListener("click", function () {
