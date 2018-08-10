b071651304677128eefa216b964949dc98df54ad
Jannape <yamileth_2005@hotmail.com>
2018-08-09 21:41:25 -0500

    registerVisited

--- src/model/registerVisited.js
+++ src/model/registerVisited.js
@@ -1,9 +1,13 @@
 
 window.createVisited = (inputNameVisited, optionTypeDocId, inputDocId, inputEmail, inputCompany, inputReason) => {
-    let  date = new Date();
-    let codDate = date.getTime();
-    let  fecha = date.setTime(codDate);
-    
+    const dataTime = new Date();
+    const mounth =dataTime.getMonth();
+    const year =dataTime.getFullYear();
+    const day=dataTime.getDate();
+    const hours= dataTime.getHours();
+    const minutes=dataTime.getMinutes();
+    const fullTime = day+"/"+(mounth+1)+"/"+year+"-"+hours+":"+minutes;
+     
     (firebase.database().ref().child('visiteds')).push({
      
         visitedName: inputNameVisited.value,
@@ -12,7 +16,7 @@ window.createVisited = (inputNameVisited, optionTypeDocId, inputDocId, inputEmai
         visitedEmail: inputEmail.value,
         visitedCompany: inputCompany.value,
         visitedReason: inputReason.value,
-        visitedFecha :fecha
+        visitedFecha :fullTime
     }).then(() => {
         console.log('error');
     })
